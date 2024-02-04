f760e81bafd21eb3a8f3f0ce3cbe5ed2f1fc3307
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-02-04 17:43:55 +0500

    completed admin home page responsive

--- src/components/adminNavbar/index.jsx
+++ src/components/adminNavbar/index.jsx
@@ -1,8 +1,18 @@
 import { Link, NavLink, Outlet } from "react-router-dom";
 import "./admin.css";
 import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaClosedCaptioning } from "react-icons/fa";
+import { useState } from "react";
 
 export const AdminNavbar = () => {
+
+  const [modal, setModal] = useState(false)
+
+  const handleModal = () => {
+    setModal((item) => !item)
+  }
+
   return (
     <>
       <div className="admin-navbar">
@@ -11,7 +21,8 @@ export const AdminNavbar = () => {
             <Link className="admin-navbar-logo-link" to={"/admin-page-scret-control/admin-home"}>
               <MdOutlineDashboardCustomize className="admin-navbar-logo"/>
             </Link>
-            <nav className="addmin-navbar-nav">
+            <nav className={modal ? "addmin-navbar-nav" : "addmin-navbar-nav-extra"}>
+              <FaClosedCaptioning className="admin-navbar-close" onClick={handleModal}/>
               <ul className="admin-navbar-list">
                 <li className="admin-navbar-item">
                   <NavLink to={"admin-home"} className="admin-navbar-item-link">
@@ -38,6 +49,7 @@ export const AdminNavbar = () => {
             </nav>
             <button className="admin-navbar-btn">Chiqish</button>
           </div>
+          <GiHamburgerMenu className="admin-navbar-hamburger"  onClick={handleModal}/>
         </div>
       </div>
       <Outlet />
